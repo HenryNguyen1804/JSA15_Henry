ac7a56d08d0a2357e5adf55a0acb8cc63c1ba5b8
Henry <quanminh18042006@gmail.com>
2021-12-04 20:10:08 +0700

    upload

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log("hello world")
+var changecolor = document.getElementById("heading");
+window.onscroll = function () {
+    changeFunction();
+};
+
+function changeFunction() {
+    if (document.documentElement.scrollTop > 535) {
+        changecolor.style.background = "white";
+    } 
+    else {
+        changecolor.style.background = "none";
+    }
+}
+
+
+
+// var abc = document.getElementById("change-color")
+// window.onscroll = {
+//     changeabc();
+// };
+// function changeabc(){
+//     if (document.documentElement.scrollTop > 535){
+//             abc.style.color = "black"
+//         }
+//         else{
+//             abc.style.color = "white"
+//         }
+// }
+
+
+
+
+
+
+
+
+
