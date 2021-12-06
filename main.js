7fb34570d09b023065d6a51f217b5fe8438c6ee6
Henry <quanminh18042006@gmail.com>
2021-12-06 08:07:29 +0700

    upload

--- main.js
+++ main.js
@@ -6,23 +6,22 @@ var list = document.querySelectorAll(".collumn-2 div a")
 
 function changeFunction() {
     if (document.documentElement.scrollTop > 350) {
-        changecolor.style.background = "white";
-        
+        changecolor.style.background = "#edf6f9";
+        changecolor.style.paddingTop="5px"
         list.forEach(item=>{
-            item.style.color="black"
+            item.style.color="#3a3b3e"
         })
+
     } 
     else {
         changecolor.style.background = "transparent";
+        changecolor.style.paddingTop="20px"
         list.forEach(item=>{
             item.style.color="white"
         })
     }
 }
 
-
-
-console.log(changecolor)
 
 
 
